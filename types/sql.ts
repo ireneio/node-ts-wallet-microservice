8fa4588786d2c4cf2afaa04e82271b20fbd033f4
Your Name <you@example.com>
2021-02-02 21:54:33 +0800

    20210202-edit-typing

--- types/sql.ts
+++ types/sql.ts
@@ -1,22 +1,4 @@
 export namespace SqlSchema {
-  interface Survey {
-    id: number | bigint,
-    type: string,
-    name: string,
-    count: number,
-    status: number,
-    created_on: string,
-    last_updated: string
-  }
-  export interface SurveyDb extends Survey {
-    record: Array<Array<string>>
-  }
-  export interface SurveyInput extends Survey {
-    record: Array<SurveyRecord>
-  }
-  export interface SurveyRecord {
-    [index: string]: string
-  }
   interface User {
     id: number | bigint,
     email: string,
